cf1528d43027c667b7208cc9a0d01ba09cda094f
Mariel García <117679688+marieeeel@users.noreply.github.com>
2025-10-05 22:42:59 -0400

    add filters, interceptors and interfaces

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './filters/all-exceptions.filter';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(new ResponseInterceptor());  
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  const port = process.env.PORT ?? 3000;
+  console.log(`Application is running on: http://localhost:${port}`);
+  await app.listen(port);
 }
 bootstrap();
