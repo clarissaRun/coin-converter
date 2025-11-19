4641b1dc38b3c5d587040ff9019818cebda20a8e
Mariel García <117679688+marieeeel@users.noreply.github.com>
2025-11-18 22:07:06 -0400

    validate users and validate currencies

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -164,11 +164,11 @@ export class UsersService {
   }
 
   async validateUser(id: string) {
-  const user = await this.prisma.user.findUnique({ where: { id } });
-  if (!user) {
-    throw new NotFoundException('User not found');
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
-  return user;
-}
 
 }
